3fe7ce72a639cbe490abef89dfd02da660f7086b
dsilentkiller <paarurawal55@gmail.com>
2022-03-15 10:59:02 +0545

    basic js

---
+++ object.js
@@ -0,0 +1,19 @@
+// Object array in javascript
+// in real life car is object.
+const car = { type: "flat", model: "500", color: "white" };
+
+// const keyword used to declare object.
+
+// const keyword with person object.
+const person = {
+  firstName: "paaru",
+  lastName: "rawal",
+  age: 50,
+  eyeColor: "gray",
+};
+
+// accessing object
+person.age;
+person.firstName;
+person.lastName;
+person.eyeColor;
---
+++ objectarray.js
@@ -0,0 +1,9 @@
+<html>
+    <head><title>hello</title></head>
+    <body>
+        <script type="text/javascript">
+            const Name ="ram"
+            console.log('Name');
+        </script>
+    </body>
+</html>
